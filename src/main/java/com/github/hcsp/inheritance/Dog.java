43138badbf73120651fdd8b4c2e044a17fa8fcce
huangjiaxi <huangjiaxi@oak-inc.com>
2019-07-05 14:22:14 +0800

    code clean

--- src/main/java/com/github/hcsp/inheritance/Dog.java
+++ src/main/java/com/github/hcsp/inheritance/Dog.java
@@ -1,16 +1,9 @@
 package com.github.hcsp.inheritance;
 
-public class Dog {
-    private String name;
-
-    public Dog(String name) {
-        this.name = name;
-    }
-
-    public void sayMyName() {
-        System.out.println("我的名字是" + name);
-    }
-
+public class Dog extends Animal{
+    Dog(String name){
+        super(name);
+        }
     public void wang() {
         System.out.println("汪");
     }
