74df9beba10bbc239a3031e1b0a1278269c6bca4
haojie <42760638+gakei@users.noreply.github.com>
2019-08-13 02:56:20 -0500

    将公用代码抽取到不同层级的父类中 (#37)
    将公用代码抽取到不同层级的父类中 (#37)

---
+++ src/main/java/com/github/hcsp/inheritance/Introduction.java
@@ -0,0 +1,14 @@
+package com.github.hcsp.inheritance;
+
+public class Introduction {
+    protected String name;
+    protected int age;
+
+    protected void sayMyName() {
+        System.out.println("我的名字是" + name);
+    }
+
+    protected void sayMyAge() {
+        System.out.println("我的年龄是" + age);
+    }
+}
