8460d9c6914ab51c20d5bd2da58339c7c4f393aa
perfectLipeng <49055265+perfectLipeng@users.noreply.github.com>
2019-09-12 10:56:28 +0800

    公用代码抽取到不同层级的父类 (#52)
    公用代码抽取到不同层级的父类 (#52)

---
+++ src/main/java/com/github/hcsp/inheritance/People.java
@@ -0,0 +1,21 @@
+package com.github.hcsp.inheritance;
+
+public class People {
+    private String name;
+    private int age;
+
+
+    public People(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void sayMyName() {
+        System.out.println("我的名字是" + name);
+    }
+
+    public void sayMyAge() {
+        System.out.println("我的年龄是" + age);
+    }
+
+}
