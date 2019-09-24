6f0becbea903b532a658fc9ac29335d214d59d3f
JiaXinWang61 <55199085+JiaXinWang61@users.noreply.github.com>
2019-09-24 22:48:27 +0800

    将公用代码抽取到不同层级的父类中 (#61)
    将公用代码抽取到不同层级的父类中 (#61)

---
+++ src/main/java/com/github/hcsp/inheritance/Person.java
@@ -0,0 +1,18 @@
+package com.github.hcsp.inheritance;
+
+public class Person extends Animal {
+
+    int age;
+
+
+    public Person(String name, int age) {
+        super(name);
+        this.age = age;
+    }
+
+
+    public void sayMyAge() {
+        System.out.println("我的年龄是" + age);
+    }
+
+}
