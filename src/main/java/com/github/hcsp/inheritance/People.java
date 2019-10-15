9eac005f6cba7af7e713767c5ff1f12bbb1a9945
mattisonzhao <50048886+mattisonzhao@users.noreply.github.com>
2019-10-15 13:05:08 +0800

    refactor to more base classes (#72)
    refactor to more base classes (#72)

---
+++ src/main/java/com/github/hcsp/inheritance/People.java
@@ -0,0 +1,15 @@
+package com.github.hcsp.inheritance;
+
+public class People extends Animal{
+    private int age ;
+
+    public People(String name,int age) {
+        super(name);
+        this.age = age;
+    }
+
+    public void sayMyAge() {
+        System.out.println("我的年龄是" + age);
+    }
+
+}
