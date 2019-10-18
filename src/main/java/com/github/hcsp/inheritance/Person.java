7dfd4c5edb8834de9bd26427a0ee72dc7f27705c
wangyujie1207 <82414199@qq.com>
2019-10-18 17:37:42 +0800

    两个继承

---
+++ src/main/java/com/github/hcsp/inheritance/Person.java
@@ -0,0 +1,16 @@
+package com.github.hcsp.inheritance;
+
+public class Person {
+    private int age;
+    private String name;
+    public Person(String name, int age) {
+        this.age = age;
+        this.name = name;
+    }
+    public void sayMyName(){
+        System.out.println("我的名字是" + name);
+    }
+    public void sayMyAge() {
+        System.out.println("我的年龄是" + age);
+    }
+}
