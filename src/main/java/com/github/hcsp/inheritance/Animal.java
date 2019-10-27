fee2a1753d69aa0ae585222f1d466eefe09375cc
Lian Yongbo <L1anyX@126.com>
2019-10-27 16:39:13 +0800

    将Cat/Dog/Boy/Girl类中的重复代码抽取到父类。 (#76)
    将Cat/Dog/Boy/Girl类中的重复代码抽取到父类。 (#76)

---
+++ src/main/java/com/github/hcsp/inheritance/Animal.java
@@ -0,0 +1,10 @@
+package com.github.hcsp.inheritance;
+
+/**
+ * @Auther:Lian
+ * @Date:2019/10/27 16:30
+ * @Description:TODO
+ * @version:1.0
+ */
+public class Animal extends Biological {
+}
---
+++ src/main/java/com/github/hcsp/inheritance/Biological.java
@@ -0,0 +1,15 @@
+package com.github.hcsp.inheritance;
+
+/**
+ * @Auther:Lian
+ * @Date:2019/10/27 16:29
+ * @Description:TODO
+ * @version:1.0
+ */
+public class Biological {
+    String name;
+
+    public void sayMyName() {
+        System.out.println("我的名字是" + name);
+    }
+}
