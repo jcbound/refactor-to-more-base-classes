33199eeb2ca41e5c0cc4405c8ecd75c5f133033f
chengdasan <56035308+chengdasan@users.noreply.github.com>
2019-11-20 15:39:07 +0800

    将公用代码抽取到不同层级父类 (#82)
    将公用代码抽取到不同层级父类 (#82)

---
+++ src/main/java/com/github/hcsp/inheritance/Animal1.java
@@ -0,0 +1,14 @@
+package com.github.hcsp.inheritance;
+
+public class Animal1 {
+    private String name;
+
+    public Animal1(String name) {
+        this.name = name;
+    }
+
+    public void sayMyName() {
+        System.out.println("我的名字是" + name);
+    }
+
+}
