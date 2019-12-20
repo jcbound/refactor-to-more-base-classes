64c39ae1c220d89c38c406db4dda85f2bbbec59f
tan661 <lucky.tanduzhi@gmail.com>
2019-12-20 16:56:17 +0800

    练习继承 (#95)
    练习继承 (#95)

---
+++ src/main/java/com/github/hcsp/inheritance/Human.java
@@ -0,0 +1,15 @@
+package com.github.hcsp.inheritance;
+
+public class Human extends Animal {
+    protected int age;
+
+    public Human(String name, int age) {
+        super(name);
+        this.age = age;
+    }
+
+    public void sayMyAge() {
+        System.out.println("我的年龄是" + age);
+    }
+
+}
