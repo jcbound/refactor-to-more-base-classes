7b86af1ff405f27f077e0fef3788d847fb5bf432
spclock <1124516293@qq.com>
2020-01-07 17:45:42 +0800

    公用代码抽取到不同层级的父类中

---
+++ src/main/java/com/github/hcsp/inheritance/AdvancedAnimal.java
@@ -0,0 +1,14 @@
+package com.github.hcsp.inheritance;
+
+public class AdvancedAnimal extends Animal{
+    int age;
+
+    public AdvancedAnimal(String name, int age) {
+        super(name);
+        this.age = age;
+    }
+
+    public void sayMyAge() {
+        System.out.println("我的年龄是" + age);
+    }
+}
