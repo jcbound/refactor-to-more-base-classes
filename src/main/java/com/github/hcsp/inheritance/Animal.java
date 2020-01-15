881fe67e625c8dda2ea01905c2e3d150aba53587
dygalo <2522486321@qq.com>
2020-01-15 16:59:31 +0800

    将公用代码抽取到不同层级的父类中

--- src/main/java/com/github/hcsp/inheritance/Animal.java
+++ src/main/java/com/github/hcsp/inheritance/Animal.java
@@ -1,7 +1,7 @@
 package com.github.hcsp.inheritance;
 
 public class Animal {
-    public String name;
+    protected String name;
 
     public void sayMyName() {
         System.out.println("我的名字是" + name);
