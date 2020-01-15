1f7adab004fef7b881915a72e5dbf7935eb43f78
dygalo <59307187+dygalo@users.noreply.github.com>
2020-01-15 17:08:38 +0800

    将公用代码抽取到不同层级的父类中 (#115)
    将公用代码抽取到不同层级的父类中 (#115)

---
+++ src/main/java/com/github/hcsp/inheritance/Human.java
@@ -0,0 +1,11 @@
+package com.github.hcsp.inheritance;
+
+public class Human extends Animal {
+
+    protected int age;
+
+
+    public void sayMyAge() {
+        System.out.println("我的年龄是" + age);
+    }
+}
