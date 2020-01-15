f151c303f74648661c64c42f5a5237bff86408d5
dygalo <2522486321@qq.com>
2020-01-15 16:57:02 +0800

    将公用代码抽取到不同层级的父类中

---
+++ src/main/java/com/github/hcsp/inheritance/Human.java
@@ -0,0 +1,11 @@
+package com.github.hcsp.inheritance;
+
+public class Human extends Animal {
+
+    public int age;
+
+
+    public void sayMyAge() {
+        System.out.println("我的年龄是" + age);
+    }
+}
