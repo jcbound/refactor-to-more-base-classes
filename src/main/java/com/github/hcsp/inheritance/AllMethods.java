bb67012b44a3c5ae0a62d8fafc9c4ed646bc8492
about-Time <vipchenpost@163.com>
2020-02-29 22:52:14 +0800

    refactor to more base classes (#133)
    refactor to more base classes (#133)

---
+++ src/main/java/com/github/hcsp/inheritance/AllMethods.java
@@ -0,0 +1,24 @@
+package com.github.hcsp.inheritance;
+
+public class AllMethods {
+    String name;
+    int age;
+
+    public AllMethods(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public AllMethods(String name) {
+        this.name = name;
+    }
+
+    public void sayMyName() {
+        System.out.println("我的名字是" + name);
+    }
+
+    public void sayMyAge() {
+        System.out.println("我的年龄是" + age);
+    }
+
+}
