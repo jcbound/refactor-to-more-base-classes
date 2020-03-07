22ccd85da63e1bbae59bc00c3f4ca2df45508a19
yanghanjie <yang_hanjie@163.com>
2020-03-07 11:48:02 -0500

    Add two base-classes Mammal and Human

--- src/main/java/com/github/hcsp/inheritance/Boy.java
+++ src/main/java/com/github/hcsp/inheritance/Boy.java
@@ -1,20 +1,10 @@
 package com.github.hcsp.inheritance;
 
-public class Boy {
-    private String name;
-    private int age;
+public class Boy extends Human{
 
     public Boy(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public void sayMyName() {
-        System.out.println("我的名字是" + name);
-    }
-
-    public void sayMyAge() {
-        System.out.println("我的年龄是" + age);
+        setName(name);
+        setAge(age);
     }
 
     public void sayBoy() {
