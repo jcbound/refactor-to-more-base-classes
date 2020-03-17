6c8a3ec8013efd78857a38b28b6f21c9589ee4ec
chenxi1916 <60290522+chenxi1916@users.noreply.github.com>
2020-03-17 14:14:06 +0800

    用继承的方式 重构代码 (#146)
    用继承的方式 重构代码 (#146)

---
+++ src/main/java/com/github/hcsp/inheritance/Zoo.java
@@ -0,0 +1,13 @@
+package com.github.hcsp.inheritance;
+
+public class Zoo {
+    String name;
+
+    public Zoo (String name) {
+        this.name = name;
+    }
+
+    public void sayMyName() {
+        System.out.println("我的名字是" + name);
+    }
+}
