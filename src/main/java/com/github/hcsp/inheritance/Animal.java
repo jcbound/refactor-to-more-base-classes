b3560b7697cf7dceffcd30dabdc3b2981f00ca1b
gofocus <go_focus@live.cn>
2020-03-23 14:37:06 +0800

    将公用代码抽取到不同层级的父类中 (#149)
    将公用代码抽取到不同层级的父类中 (#149)

---
+++ src/main/java/com/github/hcsp/inheritance/Animal.java
@@ -0,0 +1,11 @@
+package com.github.hcsp.inheritance;
+
+/**
+ * @Author: GoFocus
+ * @Date: 2020-03-23 14:31
+ * @Description:
+ */
+public class Animal extends Creature {
+
+
+}
---
+++ src/main/java/com/github/hcsp/inheritance/Creature.java
@@ -0,0 +1,16 @@
+package com.github.hcsp.inheritance;
+
+/**
+ * @Author: GoFocus
+ * @Date: 2020-03-23 14:32
+ * @Description:
+ */
+public class Creature {
+    protected String name;
+
+    public void sayMyName() {
+        System.out.println("我的名字是" + name);
+
+    }
+}
+
