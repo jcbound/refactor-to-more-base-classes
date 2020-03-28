214b168837b2e1ab0ab7d58e03a925ad6808d2e7
何佳棋 <1149752574@qq.com>
2020-03-28 19:45:57 +0800

    Java面向对象初步练习：将公用代码抽取到不同层级的父类中 (#152)
    Java面向对象初步练习：将公用代码抽取到不同层级的父类中 (#152)

---
+++ src/main/java/com/github/hcsp/inheritance/Animal.java
@@ -0,0 +1,14 @@
+package com.github.hcsp.inheritance;
+
+public class Animal {
+    private String name;
+
+    Animal(String name) {
+        this.name = name;
+    }
+
+    public void sayMyName() {
+        System.out.println("我的名字是" + name);
+    }
+
+}
