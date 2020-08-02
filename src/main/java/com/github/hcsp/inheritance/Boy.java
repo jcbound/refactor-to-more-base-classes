e3866a1a2f58c25c31cf1bdb9c539b4764caf619
godvvvvvv <lgj88508@163.com>
2020-08-02 20:04:23 +0800

    将公用代码抽取到不同父类

--- src/main/java/com/github/hcsp/inheritance/Boy.java
+++ src/main/java/com/github/hcsp/inheritance/Boy.java
@@ -1,21 +1,13 @@
 package com.github.hcsp.inheritance;
 
-public class Boy {
+public class Boy extends People {
     private String name;
     private int age;
 
     public Boy(String name, int age) {
-        this.name = name;
-        this.age = age;
+        super(name, age);
     }
 
-    public void sayMyName() {
-        System.out.println("我的名字是" + name);
-    }
-
-    public void sayMyAge() {
-        System.out.println("我的年龄是" + age);
-    }
 
     public void sayBoy() {
         System.out.println("我是一个男孩");
