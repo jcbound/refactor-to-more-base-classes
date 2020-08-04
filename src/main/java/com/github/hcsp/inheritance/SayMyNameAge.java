6a861a0b5f4c095bb5b3880e87875f46c24900d9
jslyq <61300836+jslyq@users.noreply.github.com>
2020-08-04 15:29:41 -0400

    refactor to more base classes (#185)
    refactor to more base classes (#185)

---
+++ src/main/java/com/github/hcsp/inheritance/SayMyNameAge.java
@@ -0,0 +1,15 @@
+package com.github.hcsp.inheritance;
+
+public class SayMyNameAge {
+    int age;
+    String name;
+
+    public void sayMyName() {
+        System.out.println("我的名字是" + name);
+    }
+
+    public void sayMyAge() {
+        System.out.println("我的年龄是" + age);
+    }
+
+}
