b58523b96c83475d1ef4a62eef80ea9b46246634
yafeif <74844703+yafeif@users.noreply.github.com>
2020-11-29 23:58:52 +0800

    将公用代码抽取到不同层级的父类中 (#197)
    将公用代码抽取到不同层级的父类中 (#197)

---
+++ src/main/java/com/github/hcsp/inheritance/Person.java
@@ -0,0 +1,14 @@
+package com.github.hcsp.inheritance;
+
+public class Person extends Animal {
+    private int age;
+
+    public Person(String name, int age) {
+        super(name);
+        this.age = age;
+    }
+
+    public void sayMyAge() {
+        System.out.println("我的年龄是" + age);
+    }
+}
