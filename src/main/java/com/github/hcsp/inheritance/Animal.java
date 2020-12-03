a4564f7760ba04144cf503b7aa61e54f4742a3eb
weiranyi <168578203@qq.com>
2020-12-03 19:05:28 +0800

    规范了格式

--- src/main/java/com/github/hcsp/inheritance/Animal.java
+++ src/main/java/com/github/hcsp/inheritance/Animal.java
@@ -2,12 +2,15 @@ package com.github.hcsp.inheritance;
 
 public class Animal {
     private String name;
+
     public Animal(String name) {
         this.name = name;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
+
     public void sayMyName() {
         System.out.println("我的名字是" + name);
     }
