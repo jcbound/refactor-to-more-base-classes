152f26524cf7d6ac31859f9743f66c5ec4a0ab10
skysZhang <37018639+skysZhang@users.noreply.github.com>
2021-07-09 11:54:57 +0800

    refactor to more base classes (#217)
    refactor to more base classes (#217)

--- src/main/java/com/github/hcsp/inheritance/Boy.java
+++ src/main/java/com/github/hcsp/inheritance/Boy.java
@@ -1,20 +1,10 @@
 package com.github.hcsp.inheritance;
 
-public class Boy {
-    private String name;
-    private int age;
+public class Boy extends People {
+
 
     public Boy(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public void sayMyName() {
-        System.out.println("我的名字是" + name);
-    }
-
-    public void sayMyAge() {
-        System.out.println("我的年龄是" + age);
+        super(name, age);
     }
 
     public void sayBoy() {
