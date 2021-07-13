641d7a4f8d6446a8f3a2a1e7e9cda12311560851
Real l Ez <85923172+reallEz@users.noreply.github.com>
2021-07-13 09:22:09 +0800

    抽象生物和人 (#218)
    抽象生物和人 (#218)

--- src/main/java/com/github/hcsp/inheritance/Boy.java
+++ src/main/java/com/github/hcsp/inheritance/Boy.java
@@ -1,22 +1,9 @@
 package com.github.hcsp.inheritance;
 
-public class Boy {
-    private String name;
-    private int age;
-
-    public Boy(String name, int age) {
-        this.name = name;
-        this.age = age;
+public class Boy extends 人 {
+    public Boy (String name, int age) {
+        super(name, age);
     }
-
-    public void sayMyName() {
-        System.out.println("我的名字是" + name);
-    }
-
-    public void sayMyAge() {
-        System.out.println("我的年龄是" + age);
-    }
-
     public void sayBoy() {
         System.out.println("我是一个男孩");
     }
