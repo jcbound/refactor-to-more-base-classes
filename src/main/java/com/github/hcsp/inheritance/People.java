17e6cc35cffdc8e53529d510674b82e59c402929
Xionghao Zhao <86486489+GrumpyCitizenBear@users.noreply.github.com>
2021-09-23 22:25:42 +0800

    将公用代码抽取到不同层级的父类中 (#223)
    将公用代码抽取到不同层级的父类中 (#223)

---
+++ src/main/java/com/github/hcsp/inheritance/People.java
@@ -0,0 +1,16 @@
+package com.github.hcsp.inheritance;
+
+public class People {
+    String name;
+    int age;
+
+    public void sayMyName() {
+        System.out.println("我的名字是" + name);
+    }
+
+    public void sayMyAge() {
+        System.out.println("我的年龄是" + age);
+    }
+
+
+}
