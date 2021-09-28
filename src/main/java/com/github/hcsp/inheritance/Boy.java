fa063165e922988f55e918e86d214f2fcdc134b0
codingories <34835258+codingories@users.noreply.github.com>
2021-09-28 14:09:01 +0800

    Ories (#225)
    Ories (#225)

--- src/main/java/com/github/hcsp/inheritance/Boy.java
+++ src/main/java/com/github/hcsp/inheritance/Boy.java
@@ -1,23 +1,14 @@
 package com.github.hcsp.inheritance;
 
-public class Boy {
-    private String name;
-    private int age;
+public class Boy extends Animal {
+
 
     public Boy(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public void sayMyName() {
-        System.out.println("我的名字是" + name);
-    }
-
-    public void sayMyAge() {
-        System.out.println("我的年龄是" + age);
+        super(name, age);
     }
 
     public void sayBoy() {
         System.out.println("我是一个男孩");
     }
+
 }
