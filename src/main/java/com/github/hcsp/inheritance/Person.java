91be6959a85fc0b9dc82a53b749d63994e12c844
MongieLee <le30n@outlook.com>
2021-10-26 09:26:01 +0800

    将公用代码抽取到不同层级的父类中 (#230)
    将公用代码抽取到不同层级的父类中 (#230)

---
+++ src/main/java/com/github/hcsp/inheritance/Person.java
@@ -0,0 +1,13 @@
+package com.github.hcsp.inheritance;
+
+public class Person extends Animal{
+    private Integer age;
+    public Person(String name,Integer age){
+        super(name);
+        this.age = age;
+    }
+
+    public void sayMyAge() {
+        System.out.println("我的年龄是" + age);
+    }
+}
