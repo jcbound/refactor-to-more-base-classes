0d8c4b629f9195060f7afe9b0a4502318a8a16aa
jcbound <784538725@qq.com>
2021-12-14 23:45:06 +0800

    将不同的类抽取到不同的父类中去

--- src/main/java/com/github/hcsp/inheritance/People.java
+++ src/main/java/com/github/hcsp/inheritance/People.java
@@ -3,10 +3,12 @@ package com.github.hcsp.inheritance;
 public class People {
     String name;
     int age;
-    public People(String MyName, int MyAge){
-        name=MyName;
-        age=MyAge;
+
+    public People(String MyName, int MyAge) {
+        name = MyName;
+        age = MyAge;
     }
+
     public void sayMyName() {
         System.out.println("我的名字是" + name);
     }
--- src/main/java/com/github/hcsp/inheritance/Pet.java
+++ src/main/java/com/github/hcsp/inheritance/Pet.java
@@ -1,11 +1,13 @@
 package com.github.hcsp.inheritance;
 
 public class Pet {
-   String name;
-   public Pet(String PetName){
-      name=PetName;
-   }
-   public void sayMyName() {
-      System.out.println("我的名字是" + name);
-   }
+    String name;
+
+    public Pet(String PetName) {
+        name = PetName;
+    }
+
+    public void sayMyName() {
+        System.out.println("我的名字是" + name);
+    }
 }
